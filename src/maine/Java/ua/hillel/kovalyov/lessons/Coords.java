2d9e40af5a70e7fa7dee6a986d8cfc5ebcbe8a29
Kovalyov <Kovalyovgmail.com>
2023-07-11 00:25:30 +0300

    Test commit

--- src/maine/Java/ua/hillel/kovalyov/lessons/Coords.java
+++ src/maine/Java/ua/hillel/kovalyov/lessons/Coords.java
@@ -2,11 +2,26 @@ package ua.hillel.kovalyov.lessons;
 
 public class Coords {
     public static void main(String[] args) {
-        double longitude = 74.044502;
-        double latitude = 40.689247;
-        char specSymbol = '\u00B0';
-        char specSymbol2 = '\'';
-        System.out.println("Longitude: - " + longitude + " Latitude: - " + latitude);
-        System.out.println("DMS Lat: 40" + specSymbol + "41" + specSymbol2 + " 21.2892'' N DMS Long: 74" + specSymbol + " 2" + specSymbol2 + " 40.2072'' W");
+        //double longitude = 74.044502;
+        //double latitude = 40.689247;
+        //char specSymbol = '\u00B0';
+        //char specSymbol2 = '\'';
+        //System.out.println("Longitude: - " + longitude + " Latitude: - " + latitude);
+        //System.out.println("DMS Lat: 40" + specSymbol + "41" + specSymbol2 + " 21.2892'' N DMS Long: 74" + specSymbol + " 2" + specSymbol2 + " 40.2072'' W");
+       // int a =10;
+       // int b = 5;
+        //int result = ++a - b++ - a++ + ++a;
+
+//       System.out.println("result =" + result);
+//        System.out.println("a = " +a);
+//        System.out.println("b = "+b);
+        ///int a = 700;
+        //int b = 2000;
+        //int c = 3500;
+        System.out.println(Math.pow(2,5));
+
     }
+
+
+
 }
