78c5e578e350e49e3ad5566576de12f1b8ca1bae
Kovalyov <Kovalyovgmail.com>
2023-07-11 00:33:23 +0300

    Test commit

--- src/maine/Java/ua/hillel/kovalyov/homeworks/HomeWorks3.java
+++ src/maine/Java/ua/hillel/kovalyov/homeworks/HomeWorks3.java
@@ -3,5 +3,6 @@ package ua.hillel.kovalyov.homeworks;
 public class HomeWorks3 {
     public static void main(String[] args) {
         System.out.println("Denis good teacher");
+        System.out.println("I'm a java developer");
     }
 }
