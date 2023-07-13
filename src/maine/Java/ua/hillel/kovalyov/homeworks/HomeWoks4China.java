a65cc86598d1765d93f5a234d653a9766829e2e9
Kovalyov <Kovalyovgmail.com>
2023-07-13 13:52:40 +0300

    Test commit

---
+++ src/maine/Java/ua/hillel/kovalyov/homeworks/HomeWoks4China.java
@@ -0,0 +1,24 @@
+package ua.hillel.kovalyov.homeworks;
+
+public class HomeWoks4China {
+    public static void main(String[] args) {
+        String dynasty = "Li";
+        int warrior = 13;
+        int shooter = 24;
+        int rider = 46;
+        int totalWarriors = 860;
+        String dynasty2 = "Min";
+        int warrior2 = 9;
+        int shooter2 = 35;
+        int rider2 = 12;
+        int totalWarriors2 = (int) (totalWarriors * 1.5);
+
+        System.out.println("total power " + dynasty + " = " + ((warrior + shooter + rider) * totalWarriors));
+        System.out.println("total power " + dynasty2 + " = " + ((warrior2 + shooter2 + rider2) * totalWarriors2));
+
+        System.out.printf("total power %s = %d %n", dynasty, ((warrior + shooter + rider) * totalWarriors));
+        System.out.printf("total power %s = %d %n", dynasty2, ((warrior2 + shooter2 + rider2) * totalWarriors2));
+    }
+}
+
+
