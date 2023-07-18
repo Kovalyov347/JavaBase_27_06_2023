d4a9afc68b7ed04ddb385f009275a2e8f32b3671
Kovalyov <Kovalyovgmail.com>
2023-07-18 17:59:13 +0300

    Test commit

--- src/maine/Java/ua/hillel/kovalyov/homeworks/HomeWorks6.java
+++ src/maine/Java/ua/hillel/kovalyov/homeworks/HomeWorks6.java
@@ -11,37 +11,87 @@ public class HomeWorks6 {
         System.out.println("First team: ");
         String aTeam = scanner.nextLine();
         System.out.printf("Team member #1 of %s killed: ", aTeam);
-        a1 = scanner.nextInt();
+        if (scanner.hasNextInt()) {
+            a1 = scanner.nextInt();
+        } else {
+            System.out.println("Invalid input. Please enter an integer.");
+            return;
+        }
         scanner.nextLine();
         System.out.printf("Team member #2 of %s killed: ", aTeam);
-        a2 = scanner.nextInt();
+        if (scanner.hasNextInt()) {
+            a2 = scanner.nextInt();
+        } else {
+            System.out.println("Invalid input. Please enter an integer.");
+            return;
+        }
         scanner.nextLine();
         System.out.printf("Team member #3 of %s killed: ", aTeam);
-        a3 = scanner.nextInt();
+        if (scanner.hasNextInt()) {
+            a3 = scanner.nextInt();
+        } else {
+            System.out.println("Invalid input. Please enter an integer.");
+            return;
+        }
         scanner.nextLine();
         System.out.printf("Team member #4 of %s killed: ", aTeam);
-        a4 = scanner.nextInt();
+        if (scanner.hasNextInt()) {
+            a4 = scanner.nextInt();
+        } else {
+            System.out.println("Invalid input. Please enter an integer.");
+            return;
+        }
         scanner.nextLine();
         System.out.printf("Team member #5 of %s killed: ", aTeam);
-        a5 = scanner.nextInt();
+        if (scanner.hasNextInt()) {
+            a5 = scanner.nextInt();
+        } else {
+            System.out.println("Invalid input. Please enter an integer.");
+            return;
+        }
         scanner.nextLine();
 
         System.out.println("Second team: ");
         String bTeam = scanner.nextLine();
         System.out.printf("Team member #1 of %s killed: ", bTeam);
-        b1 = scanner.nextInt();
+        if (scanner.hasNextInt()) {
+            b1 = scanner.nextInt();
+        } else {
+            System.out.println("Invalid input. Please enter an integer.");
+            return;
+        }
         scanner.nextLine();
         System.out.printf("Team member #2 of %s killed: ", bTeam);
-        b2 = scanner.nextInt();
+        if (scanner.hasNextInt()) {
+            b2 = scanner.nextInt();
+        } else {
+            System.out.println("Invalid input. Please enter an integer.");
+            return;
+        }
         scanner.nextLine();
         System.out.printf("Team member #3 of %s killed: ", bTeam);
-        b3 = scanner.nextInt();
+        if (scanner.hasNextInt()) {
+            b3 = scanner.nextInt();
+        } else {
+            System.out.println("Invalid input. Please enter an integer.");
+            return;
+        }
         scanner.nextLine();
         System.out.printf("Team member #4 of %s killed: ", bTeam);
-        b4 = scanner.nextInt();
+        if (scanner.hasNextInt()) {
+            b4 = scanner.nextInt();
+        } else {
+            System.out.println("Invalid input. Please enter an integer.");
+            return;
+        }
         scanner.nextLine();
         System.out.printf("Team member #5 of %s killed: ", bTeam);
-        b5 = scanner.nextInt();
+        if (scanner.hasNextInt()) {
+            b5 = scanner.nextInt();
+        } else {
+            System.out.println("Invalid input. Please enter an integer.");
+            return;
+        }
         scanner.nextLine();
         double avgScoreA = ((double) (a1 + a2 + a3 + a4 + a5)) / 5;
         double avgScoreB = ((double) (b1 + b2 + b3 + b4 + b5)) / 5;
@@ -50,9 +100,9 @@ public class HomeWorks6 {
                         "Total score of team %s = %f\n",
                 aTeam, avgScoreA, bTeam, avgScoreB);
         if (avgScoreA > avgScoreB) {
-            System.out.printf("Team %s win :) \n", aTeam);
+            System.out.printf("Team %s win 🙂 \n", aTeam);
         } else if (avgScoreA < avgScoreB) {
-            System.out.printf("Team %s win :) \n", bTeam);
+            System.out.printf("Team %s win 🙂 \n", bTeam);
         } else {
             System.out.println("Draw win :)))");
         }
